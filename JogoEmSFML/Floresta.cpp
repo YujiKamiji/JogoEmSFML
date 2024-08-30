35af65192b0105b60a1691bcd53151d560855920
Enzo <enzonkira@gmail.com>
2024-08-30 18:45:42 -0300

    Aleatoriedade de inimigos na floresta adicionado

--- JogoEmSFML/Floresta.cpp
+++ JogoEmSFML/Floresta.cpp
@@ -57,23 +57,42 @@ namespace Fases {
 	void Floresta::salvar() {}
 
 	void Floresta::criarNinjaGarras() {
-		NinjaGarras* ng1 = new NinjaGarras(sf::Vector2f(1000, 1000), sf::Vector2f(50.0, 100.0), p1, p2);
-		NinjaGarras* ng2 = new NinjaGarras(sf::Vector2f(4200, 800), sf::Vector2f(50.0, 100.0), p1, p2);
-		NinjaGarras* ng3 = new NinjaGarras(sf::Vector2f(8000, 600), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaGarras* ng1 = new NinjaGarras(sf::Vector2f(2200, 900), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaGarras* ng2 = new NinjaGarras(sf::Vector2f(5700, 1000), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaGarras* ng3 = new NinjaGarras(sf::Vector2f(7900, 1000), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaGarras* ng4 = new NinjaGarras(sf::Vector2f(8800, 1100), sf::Vector2f(50.0, 100.0), p1, p2);
 
 		entidades.adicionar(ng1);
 		entidades.adicionar(ng2);
 		entidades.adicionar(ng3);
+		entidades.adicionar(ng4);
 
 		pGC->incluirPersonagem(ng1);
 		pGC->incluirPersonagem(ng2);
 		pGC->incluirPersonagem(ng3);
+		pGC->incluirPersonagem(ng4);
+
+		int rng = rand() % 2;
+		if (rng) {
+			NinjaGarras* ng5 = new NinjaGarras(sf::Vector2f(4600, 1200), sf::Vector2f(50.0, 100.0), p1, p2);
+			entidades.adicionar(ng5);
+			pGC->incluirPersonagem(ng5);
+		}
+
+		rng = rand() % 2;
+		if (rng) {
+			NinjaGarras* ng6 = new NinjaGarras(sf::Vector2f(11400, 600), sf::Vector2f(50.0, 100.0), p1, p2);
+			entidades.adicionar(ng6);
+			pGC->incluirPersonagem(ng6);
+		}
+
 	}
 
 	void Floresta::criarNinjaShuriken() {
-		NinjaShuriken* ns1 = new NinjaShuriken(sf::Vector2f(1200, 1000), sf::Vector2f(50.0, 100.0), p1, p2);
-		NinjaShuriken* ns2 = new NinjaShuriken(sf::Vector2f(5000, 600), sf::Vector2f(50.0, 100.0), p1, p2);
-		NinjaShuriken* ns3 = new NinjaShuriken(sf::Vector2f(9000, 500), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaShuriken* ns1 = new NinjaShuriken(sf::Vector2f(2700, 800), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaShuriken* ns2 = new NinjaShuriken(sf::Vector2f(6200, 800), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaShuriken* ns3 = new NinjaShuriken(sf::Vector2f(9300, 700), sf::Vector2f(50.0, 100.0), p1, p2);
+		NinjaShuriken* ns4 = new NinjaShuriken(sf::Vector2f(11000, 700), sf::Vector2f(50.0, 100.0), p1, p2);
 
 		entidades.adicionar(ns1);
 		entidades.adicionar(ns1->getAtaque());
@@ -81,14 +100,46 @@ namespace Fases {
 		entidades.adicionar(ns2->getAtaque());
 		entidades.adicionar(ns3);
 		entidades.adicionar(ns3->getAtaque());
+		entidades.adicionar(ns4);
+		entidades.adicionar(ns4->getAtaque());
 
 		pGC->incluirPersonagem(ns1);
 		pGC->incluirPersonagem(ns2);
 		pGC->incluirPersonagem(ns3);
+		pGC->incluirPersonagem(ns4);
 
 		pGC->incluirProjetil(ns1->getAtaque());
 		pGC->incluirProjetil(ns2->getAtaque());
 		pGC->incluirProjetil(ns3->getAtaque());
+		pGC->incluirProjetil(ns4->getAtaque());
+
+
+		int rng = rand() % 2;
+		if (rng) {
+			NinjaShuriken* ns5 = new NinjaShuriken(sf::Vector2f(1300, 1100), sf::Vector2f(50.0, 100.0), p1, p2);
+			entidades.adicionar(ns5);
+			entidades.adicionar(ns5->getAtaque());
+			pGC->incluirPersonagem(ns5);
+			pGC->incluirProjetil(ns5->getAtaque());
+		}
+
+		rng = rand() % 3;
+		if (rng == 0) {
+			NinjaShuriken* ns6 = new NinjaShuriken(sf::Vector2f(5000, 1100), sf::Vector2f(50.0, 100.0), p1, p2);
+			entidades.adicionar(ns6);
+			entidades.adicionar(ns6->getAtaque());
+			pGC->incluirPersonagem(ns6);
+			pGC->incluirProjetil(ns6->getAtaque());
+		}
+
+		rng = rand() % 3;
+		if (rng < 2) {
+			NinjaShuriken* ns7 = new NinjaShuriken(sf::Vector2f(9700, 700), sf::Vector2f(50.0, 100.0), p1, p2);
+			entidades.adicionar(ns7);
+			entidades.adicionar(ns7->getAtaque());
+			pGC->incluirPersonagem(ns7);
+			pGC->incluirProjetil(ns7->getAtaque());
+		}
 	}
 
 	void Floresta::criarPlataforma() {}
