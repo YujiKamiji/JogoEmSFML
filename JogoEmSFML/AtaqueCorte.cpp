10b84616da92eca04fd86752ec0bcf4a03d1f244
Enzo <enzonkira@gmail.com>
2024-09-01 14:21:41 -0300

    Animação do Ataque Corte

--- JogoEmSFML/AtaqueCorte.cpp
+++ JogoEmSFML/AtaqueCorte.cpp
@@ -2,10 +2,12 @@
 
 namespace Entidades {
 	AtaqueCorte::AtaqueCorte(sf::Vector2f pos, sf::Vector2f tam) :
-		Projetil(pos, tam), danoDado(false)
+		Projetil(pos, tam), danoDado(false), corpoAnimado(150, 0, 150, 75)
 	{
 		dano = 10;
 		duracao = 200;
+		textura = pGG->carregarTextura("AtaqueCorte.png");
+		corpo.setTexture(textura);
 	}
 
 	AtaqueCorte::~AtaqueCorte() {}
@@ -21,12 +23,20 @@ namespace Entidades {
 			else
 				dano = 10;
 
+			if (duracao <= 133.3 && duracao >= 66.6)
+				corpoAnimado.left = 150;
+			if (duracao <= 66.6)
+				corpoAnimado.left = 300;
+
 			if (duracao <= 0)
 			{
 				ativo = false;
 				danoDado = false;
 				duracao = 200;
+				corpoAnimado.left = 0;
 			}
+
+			corpo.setTextureRect(corpoAnimado);
 		}
 	}
 
